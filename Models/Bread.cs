c711c72499e53832fd5a75774215a0f4c6102c31
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-02-28 10:53:13 -0800

    add 'amount' property to Bread.cs

--- Models/Bread.cs
+++ Models/Bread.cs
@@ -3,10 +3,11 @@ namespace PierresBakery.Models
   public class Bread
   {
     public int Price { get; set; }
+    public int Amount {get; set; }
 
-    public Bread(int price)
+    public Bread(int price, int amount)
     {
       Price = price;
-    }
+    } 
   }
 }
