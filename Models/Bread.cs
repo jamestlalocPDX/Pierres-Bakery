588802832693336c1c2a7855fdaf377d4675be91
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-02-28 15:00:30 -0800

    apply abstraction to calculate bread price based on deal provided in Bread.cs

--- Models/Bread.cs
+++ Models/Bread.cs
@@ -5,9 +5,30 @@ namespace PierresBakery.Models
     public int Price { get; set; }
     public int Amount {get; set; }
 
-    public Bread(int price, int amount)
+    public int Total { get; set; }
+
+    public Bread(int price, int amount, int total)
     {
       Price = price;
-    } 
+      Amount = amount;
+      Total = total;
+    }
+
+    public void breadTotal()
+    {
+      calculateBreadPrice();
+    }
+
+    private void calculateBreadPrice()
+    {
+      if (hearty.Amount == 3)
+      {
+        hearty.Total = 5;
+      }
+      else
+      {
+        hearty.Total = (hearty.Price * hearty.Amount);
+      }
+    }
   }
 }
