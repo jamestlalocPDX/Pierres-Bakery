2c2792b7ae6384c321d7b336f6ed4a5ecb964e96
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-02-28 10:13:43 -0800

    add namespace directive, with basic UI structure.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,15 @@ namespace PierresBakery
   {
     public static void Main()
     {
-      
+      Console.WriteLine("Hello, world! Welcome to Pierre's Bakery! Are you interested in buying any of our scrumptious loaves of bread, or any of our fine pastries today? (Yes/No)");
+      string userResponse = Console.ReadLine().ToLower();
+
+      if (userResponse == "no")
+      {
+        Console.WriteLine("--------------------------");
+        Console.WriteLine("Thank you for stopping by!");
+      }
+
     }
   }
 }
