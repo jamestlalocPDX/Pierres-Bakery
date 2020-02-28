69f201531700a1067d8d06055845d07744ca40ab
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-02-28 14:44:16 -0800

    attempt to use abstraction in Pastry.cs file.

--- Models/Pastry.cs
+++ Models/Pastry.cs
@@ -4,11 +4,30 @@ namespace PierresBakery.Models
   {
     public int Price { get; set; }
     public int Amount {get; set; }
+    public int Total { get; set; }
 
-    public Pastry(int price, int amount)
+    public Pastry(int price, int amount, int total)
     {
       Price = price;
       Amount = amount;
+      Total = total;
+    }
+
+    public void pastryTotal()
+    {
+      calculatePastryPrice();
+    }
+
+    private void calculatePastryPrice()
+    {
+     if (Amount == 3)
+      {
+        Total = 5;
+      }
+      else
+      {
+        Total = (Price * Amount);
+      }
     }
   }
 }
