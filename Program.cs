060cf41d4a7ab603069cf70409c522300bf73e5d
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-02-28 08:30:15 -0800

    add basic UI structure in Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using PierresBakery.Models;
+
+namespace PierresBakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      
+    }
+  }
+}
