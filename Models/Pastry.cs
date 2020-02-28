518a4b9f0942b0d81aeef2e3e535311569a06598
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-02-28 10:55:01 -0800

    add 'amount' property and key-value to Pastry.cs

--- Models/Pastry.cs
+++ Models/Pastry.cs
@@ -3,10 +3,12 @@ namespace PierresBakery.Models
   public class Pastry
   {
     public int Price { get; set; }
+    public int Amount {get; set; }
 
-    public Pastry(int price)
+    public Pastry(int price, int amount)
     {
       Price = price;
+      Amount = amount;
     }
   }
 }
