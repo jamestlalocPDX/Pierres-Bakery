d12eb4c668671f826305a2ef9a9fee24a4bc8150
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-02-28 11:32:40 -0800

    add instances of bread and pastry.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ namespace PierresBakery
   {
     public static void Main()
     {
+      Bread customer = new Bread (5, 0);
+      Pastry customer = new Pastry (2, 0);
+      
       Console.WriteLine("Hello, world! Welcome to Pierre's Bakery! Are you interested in buying any of our scrumptious loaves of bread, or any of our fine pastries today? (Yes/No)");
       string userResponse = Console.ReadLine().ToLower();
 
