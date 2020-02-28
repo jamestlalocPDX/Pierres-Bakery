9ff56874e34a32152713e7f1a3b4e415e27d554e
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-02-28 14:57:44 -0800

    successfully use abstraction to create a method to calculate pastry price based on the deal provided.

--- Program.cs
+++ Program.cs
@@ -8,10 +8,10 @@ namespace PierresBakery
   {
     public static void Main()
     {
-      Bread customer = new Bread (5, 0);
-      Pastry customer = new Pastry (2, 0);
-      
-      Console.WriteLine("Hello, world! Welcome to Pierre's Bakery! Are you interested in buying any of our scrumptious loaves of bread, or any of our fine pastries today? (Yes/No)");
+      Bread hearty = new Bread (5, 0, 0);
+      Pastry tasty = new Pastry (2, 0, 0);
+
+      Console.WriteLine("Hello, world! Welcome to Pierre's Bakery! Are you interested in buying any of our hearty loaves of bread, or any of our tasty pastries today? (Yes/No)");
       string userResponse = Console.ReadLine().ToLower();
 
       if (userResponse == "no")
@@ -20,12 +20,22 @@ namespace PierresBakery
         Console.WriteLine("Thank you for stopping by!");
         Console.WriteLine("See you next time!");
       }
-      else{
+      else
+      {
         Console.WriteLine("-------------------------------------");
         Console.WriteLine("Wonderful! Here are today's specials.");
         Console.WriteLine("-------------------------------------");
         Console.WriteLine("Bread: Buy 2 hearty loaves, and you get 1 for free!");
-        Console.WriteLine("Pastry: Buy 1 mouth-watering pastry for $2, or 3 for $5!");
+        Console.WriteLine("Pastry: Buy 1 tasty pastry for $2, or 3 for $5!");
+        Console.WriteLine("How many loaves of hearty bread would you like?");
+        int customerBreadAmount = int.Parse(Console.ReadLine());
+        hearty.Amount = customerBreadAmount;
+        Console.WriteLine("How many tasty pastries would you like?");
+        int customerPastryAmount = int.Parse(Console.ReadLine());
+        tasty.Amount = customerPastryAmount;
+        tasty.pastryTotal();
+        Console.WriteLine(tasty.Total);
+
       }
 
     }
