b9324b1e6fe99588c64e4b943258ef442c1637cd
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-02-28 16:01:21 -0800

    add order total functionality to Program.cs

--- Program.cs
+++ Program.cs
@@ -25,16 +25,25 @@ namespace PierresBakery
         Console.WriteLine("-------------------------------------");
         Console.WriteLine("Wonderful! Here are today's specials.");
         Console.WriteLine("-------------------------------------");
-        Console.WriteLine("Bread: Buy 2 hearty loaves, and you get 1 for free!");
+        Console.WriteLine("Bread: Buy 1 for $5, or buy 2 hearty loaves, and you get 1 for free!");
         Console.WriteLine("Pastry: Buy 1 tasty pastry for $2, or 3 for $5!");
+        
         Console.WriteLine("How many loaves of hearty bread would you like?");
         int customerBreadAmount = int.Parse(Console.ReadLine());
         hearty.Amount = customerBreadAmount;
+
         Console.WriteLine("How many tasty pastries would you like?");
         int customerPastryAmount = int.Parse(Console.ReadLine());
         tasty.Amount = customerPastryAmount;
-        tasty.pastryTotal();
-        Console.WriteLine(tasty.Total);
+
+        tasty.pastryBag();
+        hearty.breadBag();
+
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine("Your order is ready! You have received:");
+        Console.WriteLine("Bread: " + hearty.Amount + " hearty loaves for " + hearty.Total);
+        Console.WriteLine("Pastry: " + tasty.Amount + " tasty pastries for " + tasty.Total);
+        Console.WriteLine(tasty.Total + hearty.Total);
 
       }
 
